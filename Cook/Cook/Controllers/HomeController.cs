5680585071443eed30f33f32c9ce998418ba36fb
Bishal Heuju <b.heuju@yahoo.com>
2017-01-02 12:19:50 +0545

    Search recipe done

--- Cook/Cook/Controllers/HomeController.cs
+++ Cook/Cook/Controllers/HomeController.cs
@@ -14,12 +14,17 @@ namespace Cook.Controllers
         public static List<Recipe> recipeList = new List<Recipe>();
 
         //GET: home/index
-        public ActionResult Index()
+        public ActionResult Index(string searchString)
         {
             SqlConnect recipeLoader = new SqlConnect();
             recipeLoader.retriveData("select * from Recipe");
 
             recipeList = RecipeAccess.getInstance().getRecipeList(recipeLoader.sqlTable);
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                recipeList = recipeList.Where(s => s.name.ToLower().Contains(searchString.ToLower())).ToList<Recipe>();
+            }
 
             //commented code
             //int recipeCount = recipeLoader.sqlTable.Rows.Count;
